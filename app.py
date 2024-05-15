22ccd3194e9fb3b6c97ec65e66ed5636b0478977
Davide Gaiotto <gaiottodavide1@gmail.com>
2024-05-15 12:01:27 +0200

    Added the base version of the site

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask("RovIR WEB")
 
 GPIO.setmode(GPIO.BCM)
 
+#remove
+GPIO.setup(7,GPIO.OUT)
+
 @app.route("/<path:p>")
 def serve_static(p):
     return app.send_static_file(p)
@@ -14,26 +17,34 @@ def serve_page():
     return serve_static("index.html")
 
 @app.route("/Forward")
-def api_accendi():
+def api_forward():
     #Code that make the rover go Forward
-    pass
+    GPIO.output(7,GPIO.HIGH)
+    return Response("OK", 
+                    mimetype="text/plain; charset=utf-8", 
+                    headers={"Cache-Control":"no-cache"})
+
 
 @app.route("/Back")
-def api_spegni():
+def api_back():
     #doce that make the rover go back
-    pass
+    GPIO.output(7,GPIO.LOW)
+    return Response("OK", 
+                    mimetype="text/plain; charset=utf-8", 
+                    headers={"Cache-Control":"no-cache"})
+
 
 @app.route("/Stop")
-def api_accendi():
+def api_stop():
     #Code that make the rover stop
     pass
 
 @app.route("/TurnRight")
-def api_spegni():
+def api_turnRight():
     #doce that make the rover turn right
     pass
 
 @app.route("/TurnLeft")
-def api_accendi():
+def api_turnLeft():
     #Code that make the rover turn left
     pass
--- static/app.js
+++ static/app.js
@@ -1,1 +1,47 @@
-//Code that contains all the functions that are used to comunicate with the app.py code
+function onForward(){
+    let x = new XMLHttpRequest()
+    x.open("GET","/Forward")
+    x.send()
+}
+
+function onBack(){
+    let x = new XMLHttpRequest()
+    x.open("GET","/Back")
+    x.send()
+}
+
+function onStop(){
+    let x = new XMLHttpRequest()
+    x.open("GET","/Stop")
+    x.send()
+}
+
+function onTurnRight(){
+    let x = new XMLHttpRequest()
+    x.open("GET","/TurnRight")
+    x.send()
+}
+
+function onTurnLeft(){
+    let x = new XMLHttpRequest()
+    x.open("GET","/TurnLeft")
+    x.send()
+}
+
+
+window.addEventListener('keydown', (e) =>{
+    switch(e.key){
+            case 'a':
+                onTurnLeft();
+                break;
+            case 'd':
+                onTurnRight();
+                break;
+            case 'w':
+                onForward();
+                break;
+            case 's':
+                onBack();
+                break;
+        }
+    });
