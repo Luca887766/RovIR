7746decf33f7211e1ebb6988ca530482fe12c19f
Luca Silva <31luca.silva@gmail.com>
2024-05-15 15:54:02 +0200

    Fixed error letter

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def api_turnRight():
     GPIO.output(PWM_RIGHT,GPIO.HIGH)
     GPIO.output(PWM_LEFT,GPIO.HIGH)
     return Response("OK", mimetype="text/plain; charset=utf-8", headers={"Cache-Control":"no-cache"})
-d
+
 @app.route("/TurnLeft")
 def api_turnLeft():
     #Code that make the rover turn left
