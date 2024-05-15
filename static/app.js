d153cb35b1a927ba3573b552542359a3a4b89e8d
Luca887766 <31luca.silva@gmail.com>
2024-05-15 17:10:14 +0200

    Fixed continuous call from the js file, to the api, with a flag

--- static/app.js
+++ static/app.js
@@ -1,31 +1,66 @@
+let forward = false;
+let back = false;
+let stop = false;
+let turnLeft = false;
+let turnRight = false;
+
 function onForward(){
-    let x = new XMLHttpRequest()
-    x.open("GET","/Forward")
-    x.send()
+    if(forward === false){    
+        let x = new XMLHttpRequest()
+        x.open("GET","/Forward")
+        x.send()
+        forward = true
+        back = false
+        turnLeft = false
+        turnRight = false
+    }
 }
 
 function onBack(){
-    let x = new XMLHttpRequest()
-    x.open("GET","/Back")
-    x.send()
+    if(back === false){
+        let x = new XMLHttpRequest()
+        x.open("GET","/Back")
+        x.send()
+        forward = false
+        back = true
+        turnLeft = false
+        turnRight = false
+    }
 }
 
 function onStop(){
     let x = new XMLHttpRequest()
     x.open("GET","/Stop")
     x.send()
+    forward = false
+    back = false
+    turnLeft = false
+    turnRight = false
+
 }
 
 function onTurnRight(){
-    let x = new XMLHttpRequest()
-    x.open("GET","/TurnRight")
-    x.send()
+    if(turnRight === false){
+        let x = new XMLHttpRequest()
+        x.open("GET","/TurnRight")
+        x.send()
+        forward = false
+        back = false
+        turnLeft = false
+        turnRight = true
+    }
 }
 
 function onTurnLeft(){
-    let x = new XMLHttpRequest()
-    x.open("GET","/TurnLeft")
-    x.send()
+    if(turnLeft === false){
+        let x = new XMLHttpRequest()
+        x.open("GET","/TurnLeft")
+        x.send()
+        forward = false
+        back = false
+        turnLeft = true
+        turnRight = false
+    }
 }
 
 window.addEventListener('keydown', (e) =>{
@@ -41,6 +76,9 @@ window.addEventListener('keydown', (e) =>{
                 break;
             case 's':
                 onBack();
+                break;
+            default:
+                stop();
                 break;
         }
 });
