b2d4925c1812834c1ff3d13f6fb13510559838d4
Riccardo Compagno <riccardo.compagno06@gmail.com>
2024-05-17 10:52:40 +0200

    Unified the turn left and turn right function into one function

--- static/app.js
+++ static/app.js
@@ -4,10 +4,10 @@ let stop = false;
 let turnLeft = false;
 let turnRight = false;
 
-function onForward(){
-    if(forward === false){    
+function onForward() {
+    if (forward === false) {
         let x = new XMLHttpRequest()
-        x.open("GET","/Forward")
+        x.open("GET", "/Forward")
         x.send()
         forward = true
         back = false
@@ -16,10 +16,10 @@ function onForward(){
     }
 }
 
-function onBack(){
-    if(back === false){
+function onBack() {
+    if (back === false) {
         let x = new XMLHttpRequest()
-        x.open("GET","/Back")
+        x.open("GET", "/Back")
         x.send()
         forward = false
         back = true
@@ -28,9 +28,9 @@ function onBack(){
     }
 }
 
-function onStop(){
+function onStop() {
     let x = new XMLHttpRequest()
-    x.open("GET","/Stop")
+    x.open("GET", "/Stop")
     x.send()
     forward = false
     back = false
@@ -39,68 +39,68 @@ function onStop(){
 
 }
 
-function onTurnRight(){
-    if(turnRight === false){
-        let x = new XMLHttpRequest()
-        x.open("GET","/TurnRight")
-        x.send()
-        forward = false
-        back = false
-        turnLeft = false
-        turnRight = true
-    }
-}
-
-function onTurnLeft(){
-    if(turnLeft === false){
-        let x = new XMLHttpRequest()
-        x.open("GET","/TurnLeft")
-        x.send()
-        forward = false
-        back = false
-        turnLeft = true
-        turnRight = false
-    }
-}
-
-window.addEventListener('keydown', (e) =>{
-    switch(e.key){
-            case 'a':
-                onTurnLeft();
-                break;
-            case 'd':
-                onTurnRight();
-                break;
-            case 'w':
-                onForward();
-                break;
-            case 's':
-                onBack();
-                break;
-            default:
-                onStop();
-                break;
+function onTurn(c) {
+    if (c === 'R') {
+        if (turnRight === false) {
+            let x = new XMLHttpRequest()
+            x.open("GET", "/TurnRight")
+            x.send()
+            forward = false
+            back = false
+            turnLeft = false
+            turnRight = true
         }
+    }else if(c === 'L'){
+        if (turnLeft === false) {
+            let x = new XMLHttpRequest()
+            x.open("GET", "/TurnLeft")
+            x.send()
+            forward = false
+            back = false
+            turnLeft = true
+            turnRight = false
+        }
+    }
+}
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'a':
+            onTurn('L');
+            break;
+        case 'd':
+            onTurn('R');
+            break;
+        case 'w':
+            onForward();
+            break;
+        case 's':
+            onBack();
+            break;
+        default:
+            onStop();
+            break;
+    }
 });
 
-window.addEventListener('keyup', (e) =>{
-    if(e.key === 'w' || e.key === 'a' || e.key === 's' || e.key === 'd'){
+window.addEventListener('keyup', (e) => {
+    if (e.key === 'w' || e.key === 'a' || e.key === 's' || e.key === 'd') {
         onStop();
     }
 });
 
-function toSlide(id){
-    document.querySelectorAll("div.slide").forEach(function(e){
+function toSlide(id) {
+    document.querySelectorAll("div.slide").forEach(function (e) {
         e.classList.add("hidden")
         e.classList.remove("visible")
-        e.querySelectorAll("*").forEach(function(e2){
-            e2.tabIndex="-1"
+        e.querySelectorAll("*").forEach(function (e2) {
+            e2.tabIndex = "-1"
         })
     })
-    let d=document.getElementById(id)
+    let d = document.getElementById(id)
     d.classList.add("visible")
     d.classList.remove("hidden")
-    d.querySelectorAll("*").forEach(function(e2){
-        e2.tabIndex="-1"
+    d.querySelectorAll("*").forEach(function (e2) {
+        e2.tabIndex = "-1"
     })
 }
